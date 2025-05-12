537abfff0022268d40b9f1f95c4286dc6a31884e
Sergey Korepanov <igzhestik@mail.ru>
2025-05-12 19:44:41 +0500

    Frontend. Исправлен вывод списка папок

--- TaskTracker.Ui/src/App.tsx
+++ TaskTracker.Ui/src/App.tsx
@@ -51,7 +51,7 @@ const App: React.FC = () => {
                         ...f,
                         tasks: newTasks,
                         incompleteTaskCount: newIncompleteTaskCount }
-                    : folder));
+                    : f));
     }
 
     const createFolder = async (title: string) => {
