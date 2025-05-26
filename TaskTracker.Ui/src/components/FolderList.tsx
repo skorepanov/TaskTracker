b8d41139cc10d55202a18e0b567a18d7324d1b5a
Sergey Korepanov <igzhestik@mail.ru>
2025-05-26 23:03:13 +0500

    Frontend. Рефакторинг. FolderList: использованы items вместо children

--- TaskTracker.Ui/src/components/FolderList.tsx
+++ TaskTracker.Ui/src/components/FolderList.tsx
@@ -5,68 +5,62 @@ import { useStore } from '../stores/RootStore';
 import { formatDateTime } from '../utils';
 import Task from './Task';
 
-const { Panel } = Collapse;
-
 const FolderList: React.FC = observer(() => {
     const { taskStore, folderStore } = useStore();
 
+    const folderCollapseItems = folderStore.folders.map(f => {
+        const incompletedTasks = taskStore.incompletedTasks
+            .filter(t => t.folderId === f.id);
+
+        const completedTasks = taskStore.completedTasks
+            .filter(t => t.folderId === f.id);
+
+        const incompleteTaskCountAsText =
+            `(не выполнено: ${incompletedTasks.length} задач)`;
+
+        const modifiedDateTimeAsText = f.modifiedDateTime !== null
+            ? <><i>Изменена: {formatDateTime(f.modifiedDateTime)}</i><br /></>
+            : null;
+
+        const handleDeleteButtonClick = async () => {
+            await folderStore.deleteFolder(f);
+        }
+
+        const header =
+            <>
+                [{f.id}] {f.title} {incompleteTaskCountAsText}<br />
+                {modifiedDateTimeAsText}
+                <i>Создана: {formatDateTime(f.createdDateTime)}</i><br />
+                <Button
+                    onClick={handleDeleteButtonClick}
+                >
+                    Удалить
+                </Button>
+            </>;
+
+        const incompletedTaskComponents = incompletedTasks.map(t =>
+            <Task key={t.id} task={t} />
+        );
+
+        const completedTaskComponents = completedTasks.map(t =>
+            <Task key={t.id} task={t} />
+        );
+
+        const taskComponents =
+            <>
+                {incompletedTaskComponents}
+                {completedTaskComponents}
+            </>;
+
+        return {
+            key: f.id,
+            label: header,
+            children: taskComponents
+        };
+    });
+
     return (
-        <Collapse accordion>
-            {
-                folderStore.folders.map(f => {
-                    const incompletedTasks = taskStore.incompletedTasks
-                        .filter(t => t.folderId === f.id);
-
-                    const completedTasks = taskStore.completedTasks
-                        .filter(t => t.folderId === f.id);
-
-                    const incompleteTaskCountAsText =
-                        `(не выполнено: ${incompletedTasks.length} задач)`;
-
-                    const modifiedDateTimeAsText = f.modifiedDateTime !== null
-                        ? <><i>Изменена: {formatDateTime(f.modifiedDateTime)}</i><br /></>
-                        : null;
-
-                    const handleDeleteButtonClick = async () => {
-                        await folderStore.deleteFolder(f);
-                    }
-
-                    const header =
-                        <>
-                            [{f.id}] {f.title} {incompleteTaskCountAsText}<br />
-                            {modifiedDateTimeAsText}
-                            <i>Создана: {formatDateTime(f.createdDateTime)}</i><br />
-                            <Button
-                                onClick={handleDeleteButtonClick}
-                            >
-                                Удалить
-                            </Button>
-                        </>;
-
-                    return (
-                        <Panel
-                            key={f.id}
-                            header={header}
-                        >
-                            {
-                                incompletedTasks.map(t =>
-                                    <Task
-                                        key={t.id}
-                                        task={t}
-                                    />)
-                            }
-                            {
-                                completedTasks.map(t =>
-                                    <Task
-                                        key={t.id}
-                                        task={t}
-                                    />)
-                            }
-                        </Panel>
-                    );
-                })
-            }
-        </Collapse>
+        <Collapse accordion items={folderCollapseItems} />
     );
 });
 
