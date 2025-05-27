18afe056b579647ddba386283f5c5e0358e64ea8
Sergey Korepanov <igzhestik@mail.ru>
2025-05-27 19:30:03 +0500

    Frontend. Интегрирован Prettier

---
+++ TaskTracker.Ui/.prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    experimentalTernaries: true,
+    experimentalOperatorPosition: "start",
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    trailingComma: true,
+    bracketSpacing: true,
+    bracketSameLine: true,
+    arrowParens: "avoid",
+    endOfLine: "auto",
+    singleAttributePerLine: true,
+};
