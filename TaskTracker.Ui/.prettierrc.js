773c4cd9f3173478cbe2b8fbbe09fb0b7112dda2
Sergey Korepanov <igzhestik@mail.ru>
2025-05-27 19:57:24 +0500

    Frontend. Донастройка Prettier

--- TaskTracker.Ui/.prettierrc.js
+++ TaskTracker.Ui/.prettierrc.js
@@ -1,12 +1,11 @@
 module.exports = {
-    experimentalTernaries: true,
     experimentalOperatorPosition: "start",
     printWidth: 80,
     tabWidth: 4,
     useTabs: false,
     semi: true,
     singleQuote: false,
-    trailingComma: true,
+    trailingComma: "es5",
     bracketSpacing: true,
     bracketSameLine: true,
     arrowParens: "avoid",
